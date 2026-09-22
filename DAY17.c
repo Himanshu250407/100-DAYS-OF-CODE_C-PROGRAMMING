31565da35bd42aaa0b9f71094be2e27d1fbd6c3f
Himanshu250407 <Himanshu.41121@stu.upes.ac.in>
2026-09-22 09:10:41 +0530

    Add files via upload

---
+++ DAY17.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b, c, d, r1, r2, real, imag;
+
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    d = b * b - 4 * a * c;
+
+    if (d > 0) {
+        r1 = (-b + sqrt(d)) / (2 * a);
+        r2 = (-b - sqrt(d)) / (2 * a);
+        printf("Two distinct real roots: %.2lf %.2lf", r1, r2);
+    }
+    else if (d == 0) {
+        r1 = -b / (2 * a);
+        printf("Two equal real roots: %.2lf", r1);
+    }
+    else {
+        real = -b / (2 * a);
+        imag = sqrt(-d) / (2 * a);
+        printf("Complex roots: %.2lf + %.2lfi and %.2lf - %.2lfi",
+               real, imag, real, imag);
+    }
+
+    return 0;
+}
